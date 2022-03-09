7dc746d83e230885f4eea0d2b29899fa837865f7
keiishikura <ishikuuuuuuuuu@gmail.com>
2022-03-09 13:53:29 +0000

    [update]pretest

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,16 +29,21 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    
+    if @book.update(book_params)
+    flash[:notice]="Book was successfully updated"  
     redirect_to book_path(book.id)
-    flash[:notice]="Book was successfully created"
+    else
+    render:edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
+    flash[:error]="Book was successfully destroyed"
   end
   
   private
